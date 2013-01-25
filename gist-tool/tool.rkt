0de1bb5d660f3992b60cda5a171e64d34eef3227
Asumu Takikawa <asumu@ccs.neu.edu>
2013-01-25 14:55:37 -0500

    First attempt at showing public gists in DrRacket

--- gist-tool/tool.rkt
+++ gist-tool/tool.rkt
@@ -1,6 +1,7 @@
 #lang racket/unit
 
 (require drracket/tool
+         data/gvector
          framework
          gist
          racket/class
@@ -20,7 +21,11 @@
       (new menu-item%
            [label "Download gist..."]
            [parent edit-menu]
-           [callback (λ (i e) (do-gist this))]))
+           [callback (λ (i e) (do-gist this))])
+      (new menu-item%
+           [label "Show public gists..."]
+           [parent edit-menu]
+           [callback (λ (i e) (show-gists this))]))
 
     (super-new)))
 
@@ -37,6 +42,57 @@
       (send new-text erase)
       (send new-text insert (dict-ref files name)))))
 
+(define (show-gists frame)
+  (define gists (get-public-gists))
+  (define dialog (new gist-dialog%))
+  (send dialog add-gists gists)
+  (send dialog show #t))
+
+;; dialog% for displaying gists
+(define gist-dialog%
+  (class dialog%
+    (super-new [label "Public gists"])
+
+    ;; to contain the editors for each gist
+    (define panel (new vertical-panel% [parent this]))
+    (define main-editor (new text:basic% [auto-wrap #t]))
+    (define main-ec (new editor-canvas%
+                         [parent panel]
+                         [editor main-editor]
+                         [style '(no-hscroll)]
+                         [stretchable-width #t]))
+
+    ;; snips for each gist
+    (define snips (gvector))
+
+    ;; display a list of gists
+    (define/public (add-gists lst-of-gists)
+      (for ([gist (in-list lst-of-gists)])
+        (add-gist gist)))
+
+    ;; add a gist to the dialog
+    (define (add-gist gist-json)
+      (define gist-text (new text:basic% [auto-wrap #t]))
+      (define snip (new editor-snip% [editor gist-text]))
+      (gvector-add! snips snip)
+      (send main-editor insert snip)
+      (send main-editor insert "\n")
+      (define description
+        (let ([elem (dict-ref gist-json 'description)])
+          (if (or (eq? 'null elem) (string=? "" elem))
+              "No description provided"
+              elem)))
+      (send gist-text insert (dict-ref gist-json 'id))
+      (send gist-text insert "\n")
+      (send gist-text insert description)
+      (send gist-text lock #t))
+
+    (define/override (on-size width height)
+      (for ([snip (in-gvector snips)])
+        (send snip set-max-width width)
+        (send snip set-max-height height)))))
+
+;; Tool setup
 (define (phase1) (void))
 (define (phase2) (void))
 
